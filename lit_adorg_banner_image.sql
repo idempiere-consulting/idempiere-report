7b767a2649cc15e63e238961d14654d6b1cf8d52
Michael Longo <m.longo@metalsistem.com>
2025-05-11 11:21:22 +0200

    sql function for banner and footer images

---
+++ lit_adorg_banner_image.sql
@@ -0,0 +1,25 @@
+-- FUNCTION: adempiere.lit_adorg_banner_image(numeric)
+
+-- DROP FUNCTION IF EXISTS adempiere.lit_adorg_banner_image(numeric);
+
+CREATE OR REPLACE FUNCTION adempiere.lit_adorg_banner_image(
+	p_org_id numeric)
+    RETURNS bytea
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+DECLARE
+    result bytea;
+BEGIN
+    SELECT img.binarydata INTO result
+    FROM ad_orginfo org
+    LEFT JOIN ad_image img ON org.lit_banner_id = img.ad_image_id
+    WHERE org.ad_org_id = p_org_id;
+
+    RETURN result;
+END;
+$BODY$;
+
+ALTER FUNCTION adempiere.lit_adorg_banner_image(numeric)
+    OWNER TO adempiere;
